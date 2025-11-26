9b4cd39b3ff06b4f3bc4675bbc9c006c4b0b8cd3
stephaniechepkosgei <stephieem88@gmail.com>
2025-11-26 21:18:05 +0300

    Event listener

--- quiz.js
+++ quiz.js
@@ -6,7 +6,7 @@ const userAnswer = selectrRadio.value;
     if (userAnswer === correctAnswer) {
      document.getElementById("feedback").innerText = "Correct! Well done.";  
      } else {
-        document.getElementById("feedback").innerText = "That's incorect. Try again!";
+        document.getElementById("feedback").innerText = "That's incorrect. Try again!";
      }
      document.getElementById("submit answer").addEventListener("submit-answer",)
      function checkAnswer(addEventListener)
